ad3c9ffde3c48aa73ce3dbcec933b119bbdb19b9
luanfsouza <luanferreira7777@gmail.com>
2024-07-02 21:11:30 -0300

    praticando operador ternario em JAVA

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,12 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        String A = "uva";
+        String B = "melancia";
+        
+        String resultado = A == B ? "verdadeiro" : "falso";
+
+        System.out.println(resultado);
+    }
+}
