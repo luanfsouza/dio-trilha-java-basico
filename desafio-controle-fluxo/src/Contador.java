62bb6b3e96e9d7a9b6dc125c1018f00cc34b00a2
luanfsouza <luanferreira7777@gmail.com>
2024-07-08 16:47:52 -0300

    desafio Contador, utilizando try catch e Throws Excpetion costumizada

---
+++ desafio-controle-fluxo/src/Contador.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int primeiroValor = scanner.nextInt();
+        int segundoValor = scanner.nextInt();
+
+        try {
+            contar(primeiroValor, segundoValor);
+        } catch (ParametrosInvalidosException e) {
+            // TODO Auto-generated catch block
+            System.err.println("O segundo parâmetro deve ser maior que o primeiro: "+e);
+            e.printStackTrace();
+        }
+    }
+
+    public static void contar(int v1, int v2) throws ParametrosInvalidosException {
+        if (v1 > v2) {
+            throw new ParametrosInvalidosException();
+        } else {
+            int contagem = v2 - v1;
+            for (int x = 0; x < contagem; x++) {
+                System.out.println("Imprimindo contagem "+(x+1)+"...");
+            }
+        }
+        
+    }
+}
