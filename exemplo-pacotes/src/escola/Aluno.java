d54ff7dd486a3edd70c827e113254ef8a7e18e86
luanfsouza <luanferreira7777@gmail.com>
2024-07-15 11:56:34 -0300

    getters e setters em JAVA

---
+++ exemplo-pacotes/src/escola/Aluno.java
@@ -0,0 +1,33 @@
+package escola;
+import java.util.Objects;
+
+public class Aluno {
+    private String nome;
+    private int idade;
+    private String sexo;
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return this.idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getSexo() {
+        return this.sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+    
+}
