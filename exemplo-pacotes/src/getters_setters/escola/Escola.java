9b699e1bd08097abb70555b99128bfec78165c06
luanfsouza <luanferreira7777@gmail.com>
2024-07-22 17:05:48 -0300

    correção de caminho do package de package escola para package getters_setters.escola, e tmb remoção de import desnecessario

---
+++ exemplo-pacotes/src/getters_setters/escola/Escola.java
@@ -0,0 +1,12 @@
+package getters_setters.escola;
+
+public class Escola {
+    public static void main(String[] args) {
+        Aluno felipe = new Aluno();
+        felipe.setNome("Felipe");
+        felipe.setIdade(5);;
+
+        System.out.println("O aluno é "+felipe.getNome()+" e tem "+felipe.getIdade()+" anos");
+    }
+
+}
