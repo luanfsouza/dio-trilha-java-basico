80b9c44ae117e3e9c40785a2b148ba26af3be3dd
luanfsouza <luanferreira7777@gmail.com>
2024-07-29 12:10:06 -0300

    implementação da lista encadeada em JAVA

--- estrutura-de-dados-e-API-de-Steams/listas/src/main/java/edu/luan/listas/lista_encadeada/Main.java
+++ estrutura-de-dados-e-API-de-Steams/listas/src/main/java/edu/luan/listas/lista_encadeada/Main.java
@@ -2,6 +2,19 @@ package edu.luan.listas.lista_encadeada;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ListaEncadeada minhaLista = new ListaEncadeada<>();
+
+        System.out.println(minhaLista.size());
+        minhaLista.add("primeiro");
+        minhaLista.add("segundo");
+        minhaLista.add("terceiro");
+        minhaLista.add("quarto");
+        minhaLista.add("quinto");
+        System.out.println(minhaLista.size());
+        System.out.println(minhaLista.get(2));
+        System.out.println(minhaLista.getNo(2));
+        System.out.println(minhaLista.remove(2));
+        System.out.println(minhaLista.get(1));
+        System.out.println(minhaLista);
     }
 }
