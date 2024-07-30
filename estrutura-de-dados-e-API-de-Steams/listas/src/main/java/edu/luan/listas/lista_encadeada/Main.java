39ea50ed625a67954b46d7f67ec7c9a1eec2e963
luanfsouza <luanferreira7777@gmail.com>
2024-07-29 23:48:12 -0300

    ajuste

--- estrutura-de-dados-e-API-de-Steams/listas/src/main/java/edu/luan/listas/lista_encadeada/Main.java
+++ estrutura-de-dados-e-API-de-Steams/listas/src/main/java/edu/luan/listas/lista_encadeada/Main.java
@@ -5,16 +5,22 @@ public class Main {
         ListaEncadeada minhaLista = new ListaEncadeada<>();
 
         System.out.println(minhaLista.size());
+
         minhaLista.add("primeiro");
         minhaLista.add("segundo");
         minhaLista.add("terceiro");
         minhaLista.add("quarto");
         minhaLista.add("quinto");
+
         System.out.println(minhaLista.size());
+
         System.out.println(minhaLista.get(2));
         System.out.println(minhaLista.getNo(2));
+
         System.out.println(minhaLista.remove(2));
+
         System.out.println(minhaLista.get(1)); 
+        
         System.out.println(minhaLista); 
     }
 }
