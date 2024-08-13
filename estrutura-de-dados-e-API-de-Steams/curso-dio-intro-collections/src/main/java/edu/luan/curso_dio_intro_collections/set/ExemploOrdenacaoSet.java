fab15f9e4a8caa632a68f101ffa3f84efb014373
luanfsouza <luanferreira7777@gmail.com>
2024-08-13 17:14:38 -0300

    collection set em java

---
+++ estrutura-de-dados-e-API-de-Steams/curso-dio-intro-collections/src/main/java/edu/luan/curso_dio_intro_collections/set/ExemploOrdenacaoSet.java
@@ -0,0 +1,140 @@
+package edu.luan.curso_dio_intro_collections.set;
+
+import java.util.Comparator;
+import java.util.HashSet;
+//import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class ExemploOrdenacaoSet {
+    public static void main(String[] args) {
+        Set<Serie> minhasSeries = new HashSet<>() {
+            {
+                add(new Serie("got", "fantasia", 60));
+                add(new Serie("dark", "drama", 60));
+                add(new Serie("that '70s show", "comedia", 30));
+            }
+        };
+        for (Serie serie : minhasSeries)
+            System.out.println(serie);
+        System.out.println("------------------------------");
+
+        Set<Serie> minhasSeriesOrganizadas = new TreeSet<>(minhasSeries);
+        for (Serie serie : minhasSeriesOrganizadas)
+            System.out.println(serie);
+        System.out.println("------------------------------");
+
+        Set<Serie> minhSeries2 = new TreeSet<>(new ComparatorNomeGeneroTempoEpisodio());
+        minhSeries2.addAll(minhasSeries);
+        for(Serie serie : minhSeries2) System.out.println(serie);
+        System.out.println("------------------------------");
+    }
+}
+
+class ComparatorNomeGeneroTempoEpisodio implements Comparator<Serie> {
+
+    @Override
+    public int compare(Serie o1, Serie o2) {
+        int nome = o1.getNome().compareTo(o2.getNome());
+        if (nome != 0)
+            return nome;
+        int genero = o1.getGenero().compareTo(o2.getGenero());
+        if (genero != 0)
+            return genero;
+        return Integer.compare(o1.getTempoEpisodio(), o2.getTempoEpisodio());
+    }
+
+}
+
+class Serie implements Comparable<Serie> {
+
+    private String nome;
+    private String genero;
+    private Integer tempoEpisodio;
+
+    public Serie(String nome, String genero, Integer tempoEpisodio) {
+        this.nome = nome;
+        this.genero = genero;
+        this.tempoEpisodio = tempoEpisodio;
+    }
+
+    @Override
+    public int compareTo(Serie o) {
+        int SerieTempoEpisodio = Integer.compare(this.getTempoEpisodio(), o.getTempoEpisodio());
+        if (SerieTempoEpisodio != 0)
+            return SerieTempoEpisodio;
+
+        int SerieNome = this.getNome().compareTo(o.getNome());
+        if (SerieNome != 0)
+            return SerieNome;
+
+        return this.getGenero().compareTo(o.getGenero());
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public Integer getTempoEpisodio() {
+        return tempoEpisodio;
+    }
+
+    public void setTempoEpisodio(Integer tempoEpisodio) {
+        this.tempoEpisodio = tempoEpisodio;
+    }
+
+    public String toString() {
+        return "{" + "nome='" + this.nome + "', genero='" + this.genero + "', tempoEpisodio='"
+                + this.tempoEpisodio + "'";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + ((genero == null) ? 0 : genero.hashCode());
+        result = prime * result + ((tempoEpisodio == null) ? 0 : tempoEpisodio.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Serie other = (Serie) obj;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (genero == null) {
+            if (other.genero != null)
+                return false;
+        } else if (!genero.equals(other.genero))
+            return false;
+        if (tempoEpisodio == null) {
+            if (other.tempoEpisodio != null)
+                return false;
+        } else if (!tempoEpisodio.equals(other.tempoEpisodio))
+            return false;
+        return true;
+    }
+
+}
