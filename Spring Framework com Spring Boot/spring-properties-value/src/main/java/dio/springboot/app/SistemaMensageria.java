325836af24a42611ca58575ef837635201d710e7
luanfsouza <luanferreira7777@gmail.com>
2024-08-18 16:10:04 -0300

    .

--- Spring Framework com Spring Boot/spring-properties-value/src/main/java/dio/springboot/app/SistemaMensageria.java
+++ Spring Framework com Spring Boot/spring-properties-value/src/main/java/dio/springboot/app/SistemaMensageria.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SistemaMensageria implements CommandLineRunner {
-    @Value("${name}")
+    @Value("${name:Lucas}")
     private String nome;
     @Value("${email}}")
     private String email;
